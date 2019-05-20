e8940a6fbb902ad6d11f834e45a4f8d9b5dea4b3
TristanGueneau <43632654+trisCPE@users.noreply.github.com>
2019-05-20 20:57:13 +0200

    Update TwotoOne.js

--- TwotoOne.js
+++ TwotoOne.js
@@ -3,20 +3,17 @@ function longest(s1, s2) {
   s1=s1.split("");
   s2=s2.split("");
     
-    var new_s = [...s1,...s2];//spread to concatenate s1 and s2
-    new_s=new_s.sort();
+  var new_s = [...s1,...s2];//spread to concatenate s1 and s2
+  new_s=new_s.sort();
+  var ans_s=[];
     
-    var ans_s=[];
-    
-    for(var i=0;i<new_s.length;i++)
-    {
+  for(var i=0;i<new_s.length;i++){
         if(ans_s.indexOf(new_s[i])===-1)
         {
             ans_s.push(new_s[i]); //Add to the final array
         }
-    }
-
-ans_s=ans_s.join("");
+  }
+  ans_s=ans_s.join("");
     
-    return ans_s;
+  return ans_s;
 }
